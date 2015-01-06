fa52657c11409ada5ecec06e96f3b9ad78ff6a32
Mark Ellis <m@rkellis.com>
2015-01-06 23:17:31 +0000

    first commit, basic Catalyst structure

---
+++ lib/CatShop.pm
@@ -0,0 +1,18 @@
+package CatShop;
+use Moose;
+use namespace::autoclean;
+
+use Catalyst::Runtime 5.90;
+
+our $VERSION = 0.001000;
+$VERSION = eval $VERSION;
+
+use Catalyst qw/
+    ConfigLoader
+/;
+
+extends 'Catalyst';
+
+__PACKAGE__->setup;
+
+1;
