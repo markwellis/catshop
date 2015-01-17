24f0fb75cb58809a9765805db5acb874a7c9dc43
Graeme Lawton <graeme.lawton@setfiremedia.com>
2015-01-17 16:20:37 +0000

    Added ability to configure your own puppet config for the dev vm.
    You can use this to add your public key so you can ssh in as the
    vagrant user, copy your vim profile accross etc.

---
+++ vagrant/modules/local_config_example/manifests/init.pp
@@ -0,0 +1,9 @@
+class local_config {
+    notify{'Local config defined. Including it.':}
+
+    #
+    # If you want to include you're own puppet config fore the development 
+    # environment then copy this example module to local_config, the whole 
+    # folder, then edit this file in the copy to include your own copy.
+    #
+}
